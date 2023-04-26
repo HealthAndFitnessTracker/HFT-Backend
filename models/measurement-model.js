29a95aa1bdf01ade1dbbeb6c4dab593dd977b917
braedonb98 <braedonb98@gmail.com>
2023-04-25 18:53:34 -0600

    HFT-80 Measurement Model Done

--- models/measurement-model.js
+++ models/measurement-model.js
@@ -3,27 +3,15 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const measurementSchema = new Schema({
-  name: { type: String },
-  //ID
-  recurring: {
-    value: { type: Boolean },
-    time: { type: Number },
-    category: { type: String },
-  },
-  status: { type: String },
-  due: {
-    date: { type: Number },
-    time: { type: Number },
-  },
-  priority: { type: Number },
-  address: { type: String },
-  location: {
-    lat: { type: Number },
-    lng: { type: Number },
-  },
-  notes: { type: String, required: true },
+  weight: { type: String },
+  chest: [{ type: Int }],
+  leftArm: [{ type: Int }],
+  rightArm: [{ type: Int }],
+  stomach: [{ type: Int }],
+  hips: [{ type: Int }],
+  leftThigh: [{ type: Int }],
+  rightThigh: [{ type: Int }],
   creator: { type: mongoose.Types.ObjectId, ref: "User" },
-  users: [{ type: mongoose.Types.ObjectId, ref: "User" }],
 });
 
 module.exports = mongoose.model("Measurement", measurementSchema);
