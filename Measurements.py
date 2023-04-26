1961c5f80bcf58a8f850b37dd005b4225fd8b4f6
Noah77707 <108202645+Noah77707@users.noreply.github.com>
2023-04-26 13:11:42 -0600

    Add files via upload
    Cant seems to get git to separate my files when pushing, so I'm going to be uploading them. this is the beta version of the Measurements.py

---
+++ Measurements.py
@@ -0,0 +1,15 @@
+# Work: Noah Hoepfinger
+class Measurements():
+    def __init__(self, Weight:int, Chest:int, Left_arm:int, Right_arm:int, Stomach:int, Hips:int, Left_thigh:int, Right_thigh:int):
+        self.weight = Weight
+        self.chest = Chest
+        self.left_arm = Left_arm
+        self.right_arm = Right_arm
+        self.stomach = Stomach
+        self.hips = Hips
+        self.left_thigh = Left_thigh
+        self.right_thigh = Right_thigh
+
+def AddMeasurement():
+    
+def DeleteMeasurement():
