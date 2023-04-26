525735d3778386b62d11c680d7162aa8d98faf06
braedonb98 <braedonb98@gmail.com>
2023-04-25 19:13:38 -0600

    HFT-85

--- models/activity-model.js
+++ models/activity-model.js
@@ -2,15 +2,12 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const measurementSchema = new Schema({
-  weight: { type: String },
-  chest: [{ type: Int }],
-  leftArm: [{ type: Int }],
-  rightArm: [{ type: Int }],
-  stomach: [{ type: Int }],
-  hips: [{ type: Int }],
-  leftThigh: [{ type: Int }],
-  rightThigh: [{ type: Int }],
+const activitySchema = new Schema({
+  name: { type: String },
+  type: { type: String },
+  time: { type: String },
+  calories: { type: String },
+  quality: { type: String },
   creator: { type: mongoose.Types.ObjectId, ref: "User" },
 });
 
