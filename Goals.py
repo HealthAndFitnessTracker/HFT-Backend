7c6b027fd83ba3ae5f70ec8847180c812e498530
Noah Hoepfinger <Noah.hoepfinger@gmail.com>
2023-04-27 10:56:04 -0600

    Testing goals.py, saw noone had claimed it on jira

---
+++ Goals.py
@@ -0,0 +1,8 @@
+class Goals():
+    def __init__(self, Measurements:Measurements, Date:Date, Notes:Str):
+        self.Measurements = Measurements
+        self.Date = Date
+        self.Notes = Notes
+
+    def SetGoal():
+        
